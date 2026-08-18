e170e2e975a6bae9d8e7b0673ad08516fb4c297f
Strokkur24 <strokkur.24@gmail.com>
2026-08-18 02:14:32 +0200

    Add limit logic

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
 mod fill;
 
 use crate::fill::{FillBuild, fetch_project_versions};
+use clap::Parser;
 use reqwest::Client;
 use reqwest::header::{HeaderMap, HeaderValue};
 
 pub const FILL_ENDPOINT: &str = "https://fill.papermc.io/v3";
 pub const PROJECTS: [&str; 4] = ["paper", "folia", "velocity", "waterfall"];
 
+#[derive(clap::Parser)]
+struct Args {
+  #[arg(long)]
+  limit: Option<u32>,
+}
+
 #[tokio::main]
 async fn main() {
+  let args = Args::parse();
+
   let mut headers = HeaderMap::default();
   headers.append("User-Agent", HeaderValue::from_static("fill-build-hasher (strokkur.24@gmail.com)"));
   headers.append("accept", HeaderValue::from_static("application/json"));
@@ -16,22 +25,31 @@ async fn main() {
   let client = Client::builder().default_headers(headers).build().expect("Failed to build reqwest client.");
 
   let mut requests_count: u32 = 0;
-  let mut builds_count: u32 = 0;
-  
+  let mut all_builds: Vec<FillBuild> = Vec::new();
+
   let versions = fetch_project_versions(&client, "paper").await.unwrap();
   requests_count += 1;
+
   for version in versions {
     let builds = FillBuild::from_url(&client, "paper", version.as_str()).await.unwrap();
     requests_count += 1;
-    for build in builds {
-      let id = build.id;
-      let name = build.name;
-      let sha = build.sha256;
-      builds_count += 1;
-      println!("{id} | {name} | {sha}")
+    all_builds.extend(builds);
+
+    if args.limit.is_some() && all_builds.len() > args.limit.unwrap() as usize {
+      break;
     }
   }
-  
+
+  if args.limit.is_some() {
+    all_builds.truncate(args.limit.unwrap() as usize)
+  }
+
+  for build in all_builds {
+    let name = build.name;
+    let url = build.url;
+    println!("{name} | {url}")
+  }
+
   println!();
-  print!("Total API requests: {requests_count} | Total Paper builds: {builds_count}");
+  println!("Total API requests: {requests_count}");
 }
